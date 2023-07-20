0df6254201db4a451ba6bc55ef870089de4f9a88
Jordan Martinez <jordanalex.martinez@protonmail.com>
2023-07-20 05:45:19 -0700

    Update tests to include new APIs

--- test/Main.purs
+++ test/Main.purs
@@ -5,12 +5,13 @@ import Prelude
 import Data.Foldable (traverse_)
 import Effect (Effect)
 import Effect.Console (log, logShow)
-import Node.OS (arch, constants, cpus, devNull, endianness, eol, freemem, getCurrentProcessPriority, homedir, hostname, loadavg, networkInterfaces, release, setCurrentProcessPriority, tmpdir, totalmem, type_, uptime, userInfoSE, version)
+import Node.OS (arch, availableParallelism, constants, cpus, devNull, endianness, eol, freemem, getCurrentProcessPriority, homedir, hostname, loadavg, machine, networkInterfaces, release, setCurrentProcessPriority, tmpdir, totalmem, type_, uptime, userInfoSE, version)
 import Unsafe.Coerce (unsafeCoerce)
 
 main :: Effect Unit
 main = do
   log $ show eol
+  logShow =<< availableParallelism
   logShow =<< arch
   log $ (unsafeCoerce :: _ -> String) constants
   traverse_ logShow =<< cpus
@@ -21,6 +22,7 @@ main = do
   log =<< homedir
   log =<< hostname
   logShow =<< loadavg
+  logShow =<< machine
   logShow =<< networkInterfaces
   log =<< release
   setCurrentProcessPriority 30
